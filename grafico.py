54e458dee4bd9b75a9eab487d41349d949df2a44
barracius <rjgonzalez@miuandes.cl>
2018-10-23 16:11:11 -0300

    python

---
+++ grafico.py
@@ -0,0 +1,23 @@
+import subprocess
+import matplotlib.pyplot as plt
+
+marcos = []
+randfp = []
+r = []
+w = []
+numeros = []
+i = 2
+while i < 101:
+    tmp=subprocess.Popen(["./virtmem",str(100),str(i),"rand","sort"], stdout=subprocess.PIPE)
+    print("printing result " + str(i) + ":")
+    output = tmp.stdout.read()
+    numeros = [int(s) for s in output.split() if s.isdigit()]
+    randfp.append(numeros[0])
+    r.append(numeros[1])
+    w.append(numeros[2])
+    marcos.append(i)
+    i+=1
+
+plt.plot(marcos,randfp)
+plt.plot(marcos,r)
+plt.plot(marcos,w)
